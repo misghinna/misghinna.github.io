eabcc29b1b388569bcfa9417b33fccf94172cc7c
misghinna <75956497+misghinna@users.noreply.github.com>
2021-03-08 13:56:42 -0600

    Add files via upload

---
+++ TipCalculator/calculator.js
@@ -0,0 +1,9 @@
+
+
+function tipCalc() {
+    let subt = document.getElementByIdxtName("subtot").;
+    let tip = document.getElementById("tip");
+    let rslt = (subt.value * tip.value) / 100;
+    var res = document.getElementById("result")
+    res.innerHTML = rslt.value;
+}.value
