1f778ce7a60ed8f2763e4a531111f0cd90f83045
misghinna <75956497+misghinna@users.noreply.github.com>
2021-03-08 14:14:53 -0600

    Update calculator.js

--- TipCalculator/calculator.js
+++ TipCalculator/calculator.js
@@ -1,9 +1,7 @@
-
-
 function tipCalc() {
-    let subt = document.getElementByIdxtName("subtot").;
+    let subt = document.getElementById("subtot");
     let tip = document.getElementById("tip");
     let rslt = (subt.value * tip.value) / 100;
     var res = document.getElementById("result")
-    res.innerHTML = rslt.value;
-}.value
+    res.innerHTML = rslt;
+}
