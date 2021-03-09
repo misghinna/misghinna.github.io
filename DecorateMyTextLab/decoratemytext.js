578b75c81bb0b3aad1df71488dddcccfbfd3bb6f
misghinna <75956497+misghinna@users.noreply.github.com>
2021-03-09 14:53:49 -0600

    Update decoratemytext.js

--- DecorateMyTextLab/decoratemytext.js
+++ DecorateMyTextLab/decoratemytext.js
@@ -1,9 +1,29 @@
 
-function myAlert() {
-    // alert("Hello, world!");
+
+function helloalert() {
+    alert("Hello, world!");
+}
+function bigdeco() {
+
     document.getElementById("tf").style.fontSize = "24pt";
 }
 
 function myCheck() {
+    alert("Textbox checked")
+}
 
+function checkbold() {
+    var field = document.getElementById("tf");
+    if (document.getElementById("chb").checked == true) {
+        field.style.fontWeight = "bold";
+        field.style.color = "green";
+        field.style.textDecoration = "underline";
+        field.style.backgroundImage = " url('https://courses.cs.washington.edu/courses/cse190m/10su/labs/5/hundred-dollar-bill.jpg'";
+
+    } else {
+        field.style.fontWeight = "";
+        field.style.color = "";
+        field.style.textDecoration = "";
+        field.style.backgroundImage = "";
+    }
 }
